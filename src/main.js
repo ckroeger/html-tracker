01bc739bde33bc080784efd110d8213b6f357ba9
ckroeger <ckroeger.hh@gmail.com>
2024-07-25 21:39:09 +0200

    first version

--- src/main.js
+++ src/main.js
@@ -1,1 +1,28 @@
-console.log("Hello, World!");
+var map = L.map("map").setView([51.505, -0.09], 13);
+L.tileLayer("https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png", {
+  maxZoom: 19,
+}).addTo(map);
+
+if (navigator.geolocation) {
+  var marker, circle;
+  setInterval(() => {
+    navigator.geolocation.getCurrentPosition(onLocationFound);
+  }, 2500);
+
+  function onLocationFound(position) {
+    if (marker) {
+      map.removeLayer(marker);
+      map.removeLayer(circle);
+    }
+    console.log(position);
+    var lat = position.coords.latitude;
+    var lng = position.coords.longitude;
+    if (!marker) map.setView([lat, lng], 13);
+    marker = L.marker([lat, lng]); //.bindPopup("You are here").openPopup();
+    circle = L.circle([lat, lng], {
+      radius: position.coords.accuracy,
+    });
+    var featureGroup = L.featureGroup([marker, circle]).addTo(map);
+    map.fitBounds(featureGroup.getBounds());
+  }
+}
